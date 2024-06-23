25f17c695ba72ccabca4778e7e3305042aaee1b2
wa2l-99 <zanoutr.1999@gmail.com>
2024-06-23 21:59:15 +0200

    Activated Auditing jpa for ms patient

--- services/patient/src/main/java/com/example/medhead/PatientApplication.java
+++ services/patient/src/main/java/com/example/medhead/PatientApplication.java
@@ -2,8 +2,11 @@ package com.example.medhead;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PatientApplication {
 
 	public static void main(String[] args) {
