a4bddeafe0e4b6d07f46571e51ab724d581f582b
wa2l-99 <zanoutr.1999@gmail.com>
2024-06-23 22:44:08 +0200

    Add and configure Role entity

--- services/userAuthentificaton/src/main/java/com/example/medhead/models/Role.java
+++ services/userAuthentificaton/src/main/java/com/example/medhead/models/Role.java
@@ -6,13 +6,10 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
-
-@Table(name = "user")
 @Entity
 @Getter
 @Setter
@@ -20,34 +17,18 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
-public class User {
+@Table(name = "role")
+public class Role {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Integer id;
 
+    @Column(unique = true)
     private String nom;
 
-    private String prenom;
-
-    private LocalDate dateNaissance;
-
-    @Column(unique = true)
-    private String email;
-
-    private String sexe;
-
-    private String adresse;
-
-    private String numero;
-
-    private String password;
-
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable( name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles = new HashSet<>();
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> patients = new HashSet<>();
 
     @CreatedDate
     @Column(nullable = false, updatable = false)
@@ -56,5 +37,4 @@ public class User {
     @LastModifiedDate
     @Column(insertable = false)
     private LocalDateTime lastModifiedDate;
-
 }
