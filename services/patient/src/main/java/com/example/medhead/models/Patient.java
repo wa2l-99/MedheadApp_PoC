1c3987de450e9c39d8bde74da1e5cc6aa10d3a61
wa2l-99 <zanoutr.1999@gmail.com>
2024-06-23 22:01:17 +0200

    Delete not used importes

--- services/patient/src/main/java/com/example/medhead/models/Patient.java
+++ services/patient/src/main/java/com/example/medhead/models/Patient.java
@@ -3,14 +3,12 @@ package com.example.medhead.models;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
+
 
 @Table(name = "patient")
 @Entity
