949d77447f13c1d154b2b5ff42b8227f5a34a539
wa2l-99 <zanoutr.1999@gmail.com>
2024-06-24 00:43:35 +0200

    Ajouter RoleRepository

---
+++ services/userAuthentificaton/src/main/java/com/example/medhead/dao/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.example.medhead.dao;
+
+import com.example.medhead.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByName(String role);
+}
