b27b55546740ec0cd6b51d68c272b8bb914515c7
wa2l-99 <zanoutr.1999@gmail.com>
2024-06-23 22:40:06 +0200

    Update the service name

--- services/userAuthentificaton/src/main/java/com/example/medhead/userAuthentificaton.java
+++ services/userAuthentificaton/src/main/java/com/example/medhead/userAuthentificaton.java
@@ -3,14 +3,13 @@ package com.example.medhead;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class userAuthentificatonApplication {
+public class userAuthentificaton {
 
 	public static void main(String[] args) {
-		SpringApplication.run(userAuthentificatonApplication.class, args);
+		SpringApplication.run(userAuthentificaton.class, args);
 	}
 
 }
