248dbddeb19a94672cf04723ca1e9fe21b541580
wa2l-99 <zanoutr.1999@gmail.com>
2024-06-24 02:01:05 +0200

    Implements Authentification provider

--- services/userAuthentificaton/src/main/java/com/example/medhead/security/SecurityConfig.java
+++ services/userAuthentificaton/src/main/java/com/example/medhead/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.example.medhead.jwt.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
