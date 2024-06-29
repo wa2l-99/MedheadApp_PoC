50b24e11f7dab92229198ababaa67ae301ca0a10
wa2l-99 <zanoutr.1999@gmail.com>
2024-06-29 18:56:16 +0200

    initialiser la création du role dans la bdd au moment de démarrage de l'application

--- services/userAuthentificaton/src/main/java/com/example/medhead/userAuthentificaton.java
+++ services/userAuthentificaton/src/main/java/com/example/medhead/userAuthentificaton.java
@@ -1,7 +1,11 @@
 package com.example.medhead;
 
+import com.example.medhead.dao.RoleRepository;
+import com.example.medhead.models.Role;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -14,6 +18,14 @@ public class userAuthentificaton {
 		SpringApplication.run(userAuthentificaton.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByNom("Patient").isEmpty()) {
+				roleRepository.save(Role.builder().nom("Patient").build());
+			}
+		};
+	}
 }
 
 
