8929b89e2345670b9bda0e3547a7bf5b8f4028c1
wa2l-99 <zanoutr.1999@gmail.com>
2024-06-29 13:36:00 +0200

    Create the registrationRequest

---
+++ services/userAuthentificaton/src/main/java/com/example/medhead/util/request/RegistrationRequest.java
@@ -0,0 +1,49 @@
+package com.example.medhead.util.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+public class RegistrationRequest {
+
+    @NotBlank(message = "Le nom est obligatoire")
+    @NotEmpty(message = "Le nom est obligatoire")
+    private String nom;
+
+    @NotBlank(message = "Le prénom est obligatoire")
+    @NotEmpty(message = "Le prénom est obligatoire")
+    private String prenom;
+
+    @Past(message = "La date de naissance doit être dans le passé")
+    private LocalDate dateNaissance;
+
+    @NotBlank(message = "L'email est obligatoire")
+    @Email(message = "L'email doit être valide")
+    private String email;
+
+    @NotBlank(message = "Le sexe est obligatoire")
+    @Pattern(regexp = "^(Homme|Femme)$", message = "Le sexe doit être soit 'Homme', 'Femme'")
+    private String sexe;
+
+    @NotBlank(message = "L'adresse est obligatoire")
+    private String adresse;
+
+    @NotBlank(message = "Le numéro de téléphone est obligatoire")
+    @Pattern(regexp = "^\\+?\\d+$", message = "Le numéro de téléphone doit être valide")
+    private String numero;
+
+    @NotBlank(message = "Le mot de passe est obligatoire")
+    @Size(min = 8, message = "Le mot de passe doit contenir au moins 8 caractères")
+    private String password;
+}
