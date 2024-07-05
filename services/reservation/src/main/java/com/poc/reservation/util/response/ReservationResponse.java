f017c624f9b94b82ae1555ebad04a902f6dde4f0
wa2l-99 <zanoutr.1999@gmail.com>
2024-07-05 23:51:14 +0200

    fixes for reservation ms

--- services/reservation/src/main/java/com/poc/reservation/util/response/ReservationResponse.java
+++ services/reservation/src/main/java/com/poc/reservation/util/response/ReservationResponse.java
@@ -1,8 +1,6 @@
 package com.poc.reservation.util.response;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record ReservationResponse(
 
         Integer id,
