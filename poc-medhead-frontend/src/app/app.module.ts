ac7d336a862b8bfdfd94ff15c72bd5bb309b9af1
wa2l-99 <zanoutr.1999@gmail.com>
2024-07-14 21:09:31 +0200

    Impliment the register page

--- poc-medhead-frontend/src/app/app.module.ts
+++ poc-medhead-frontend/src/app/app.module.ts
@@ -7,11 +7,15 @@ import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common
 import { LoginComponent } from './pages/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { BlobToJsonInterceptor } from './interceptors/blob-to-json.interceptor';
+import { RegisterComponent } from './pages/register/register.component';
+import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    ActivateAccountComponent
   ],
   imports: [
     BrowserModule,
---
+++ poc-medhead-frontend/src/app/pages/register/register.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { RegistrationRequest } from '../../services/authentication_service/models';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../services/authentication_service/services';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
+})
+export class RegisterComponent {
+  errorMsg: string[] = [];
+  registerRequest: RegistrationRequest = {
+    adresse: '',
+    dateNaissance: '',
+    email: '',
+    nom: '',
+    numero: '',
+    password: '',
+    prenom: '',
+    sexe: '',
+  };
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {}
+
+  login() {
+    this.router.navigate(['login']);
+  }
+
+  register() {
+    this.errorMsg = [];
+    this.authService.register({ body: this.registerRequest })
+    .subscribe({
+      next: () => {
+        this.router.navigate(['activate_account'])
+      },
+      error: (err) => {
+        this.errorMsg = err.error.validationErrors;
+      }
+    });
+  }
+}
