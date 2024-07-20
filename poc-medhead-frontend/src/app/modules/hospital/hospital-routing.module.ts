a55b7e3ba0e4e5ab2fe78c422b752cad1981f320
wa2l-99 <zanoutr.1999@gmail.com>
2024-07-20 20:07:48 +0200

    fix the routing system to display all the compontes under the main component

--- poc-medhead-frontend/src/app/modules/hospital/hospital-routing.module.ts
+++ poc-medhead-frontend/src/app/modules/hospital/hospital-routing.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 import { SearchComponent } from './components/search/search.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MainComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'search',
+        component: SearchComponent,
+      },
+    ],
   },
-  { path: 'search', component: SearchComponent },
 ];
 
 @NgModule({
