541de27f8a7c2d65ab98b03b3ec587e0930a37b6
wa2l-99 <zanoutr.1999@gmail.com>
2024-07-27 23:02:34 +0200

    change error message in fr

--- services/userAuthentificaton/src/main/java/com/example/medhead/handler/BusinessErrorCodes.java
+++ services/userAuthentificaton/src/main/java/com/example/medhead/handler/BusinessErrorCodes.java
@@ -8,12 +8,12 @@ import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.NOT_IMPLEMENTED;
 
 public enum BusinessErrorCodes {
-    NO_CODE(0, NOT_IMPLEMENTED, "No code"),
-    INCORRECT_CURRENT_PASSWORD(300, BAD_REQUEST, "Current password is incorrect"),
-    NEW_PASSWORD_DOES_NOT_MATCH(301, BAD_REQUEST, "The new password does not match"),
-    ACCOUNT_LOCKED(302, FORBIDDEN, "User account is locked"),
-    ACCOUNT_DISABLED(303, FORBIDDEN, "User account is disabled"),
-    BAD_CREDENTIALS(304, FORBIDDEN, "Login and / or Password is incorrect"),
+    NO_CODE(0, NOT_IMPLEMENTED, "Pas de code"),
+    INCORRECT_CURRENT_PASSWORD(300, BAD_REQUEST, "Le mot de passe actuel est incorrect"),
+    NEW_PASSWORD_DOES_NOT_MATCH(301, BAD_REQUEST, "Le nouveau mot de passe ne correspond pas"),
+    ACCOUNT_LOCKED(302, FORBIDDEN, "Le compte utilisateur est verrouillé"),
+    ACCOUNT_DISABLED(303, FORBIDDEN, "Le compte utilisateur est désactivé"),
+    BAD_CREDENTIALS(304, FORBIDDEN, "Identifiant et/ou mot de passe incorrect"),
     ;
     @Getter
     private final int code;
