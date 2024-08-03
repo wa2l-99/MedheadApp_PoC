decbbccd1f92859e90771155ad3edc2cd6ba8bfd
wa2l-99 <zanoutr.1999@gmail.com>
2024-08-04 01:16:44 +0200

    add config file for reservation testes

--- services/reservation/src/test/java/com/poc/reservation/ReservationApplicationTests.java
+++ services/reservation/src/test/java/com/poc/reservation/ReservationApplicationTests.java
@@ -2,9 +2,10 @@ package com.poc.reservation;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class ReservationApplicationTests {
+@ActiveProfiles("test")class ReservationApplicationTests {
 
 	@Test
 	void contextLoads() {
