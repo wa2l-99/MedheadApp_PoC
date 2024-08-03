002a5d47dfa9e5ac0abeac0f5decacaa80d7587c
wa2l-99 <zanoutr.1999@gmail.com>
2024-08-03 20:47:37 +0200

    fix tests

--- services/userAuthentificaton/src/test/java/com/example/medhead/userAuthentificatonApplicationTests.java
+++ services/userAuthentificaton/src/test/java/com/example/medhead/userAuthentificatonApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.medhead;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "spring.profiles.active=test")
+@SpringBootTest
 class userAuthentificatonApplicationTests {
 
 	@Test
