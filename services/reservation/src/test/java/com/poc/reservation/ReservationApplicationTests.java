687f1dfa6ed09bbdb41b881271c72f8461942692
wa2l-99 <zanoutr.1999@gmail.com>
2024-08-04 01:33:45 +0200

    add config file for the notification ms tests

--- services/reservation/src/test/java/com/poc/reservation/ReservationApplicationTests.java
+++ services/reservation/src/test/java/com/poc/reservation/ReservationApplicationTests.java
@@ -5,10 +5,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")class ReservationApplicationTests {
+@ActiveProfiles("test")
+class ReservationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
