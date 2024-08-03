a39a654ac5a990ba64f43aa844357b16969aeda1
wa2l-99 <zanoutr.1999@gmail.com>
2024-08-03 23:17:19 +0200

    add a config file the hospital ms tests

--- services/hospitalManagement/src/test/java/com/poc/medhead/HospitalManagementApplicationTests.java
+++ services/hospitalManagement/src/test/java/com/poc/medhead/HospitalManagementApplicationTests.java
@@ -2,12 +2,14 @@ package com.poc.medhead;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HospitalManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
