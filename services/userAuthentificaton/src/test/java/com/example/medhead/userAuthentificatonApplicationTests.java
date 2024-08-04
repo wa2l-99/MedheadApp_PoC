ae649e25dca6d2aeea04a8b93cc300434952d951
wa2l-99 <zanoutr.1999@gmail.com>
2024-08-04 16:42:53 +0200

    Add untracked test files

---
+++ services/userAuthentificaton/src/test/java/com/example/medhead/userAuthentificatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medhead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class userAuthentificatonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
