a5abe76dbaae8aac1f8fbc7658f9eaf6f824cf93
wa2l-99 <zanoutr.1999@gmail.com>
2024-08-10 20:38:22 +0200

    Add cy test of displaying all exsiting hospitlas for the admin

---
+++ poc-medhead-frontend/cypress/e2e/listAllHospitalsForAdmin.cy.ts
@@ -0,0 +1,54 @@
+describe('Admin view: List of All hospitals', () => {
+  beforeEach(() => {
+    // Se connecter en tant qu'admin
+    cy.visit('/login');
+    cy.fixture('userAdminLogin').then((loginData) => {
+      cy.get('[name=email]').type(loginData.email);
+      cy.get('[name=password]').type(loginData.password);
+      cy.contains('button', 'Connexion').click();
+      cy.location('pathname').should('equal', '/hospital');
+      cy.get('.btn-manage-hospitals').should('be.visible');
+    });
+  });
+
+  it('Visits the All hospitals page and checks hospital list', () => {
+    // Naviguer vers la page des hôpitaux
+    cy.get('.btn-manage-hospitals').click();
+    cy.location('pathname').should('equal', '/hospitals');
+    cy.contains('Liste des hôpitaux').should('be.visible');
+
+    // Vérifier que la liste des hôpitaux est visible
+    cy.get('.hospital-card').should('have.length.greaterThan', 0);
+
+    cy.get('.hospital-card').each(($card) => {
+      cy.wrap($card).find('h5').should('contain.text', 'Hôpital');
+      cy.wrap($card).find('.fa-pen-to-square').should('be.visible');
+      cy.wrap($card).find('.fa-trash-can').should('be.visible');
+      cy.wrap($card).find('.fa-location-dot').should('be.visible');
+      cy.wrap($card).find('.fa-envelope').should('be.visible');
+      cy.wrap($card).find('.fa-hand-holding-medical').should('be.visible');
+      cy.wrap($card).find('.fa-bed').should('be.visible');
+      cy.wrap($card).find('.fa-pencil').should('be.visible');
+    });
+
+    // Vérifier la pagination
+    cy.get('.pagination').should('be.visible');
+    cy.get('.pagination .page-item').should('have.length.greaterThan', 1);
+
+    // Stocker le texte de la première carte d'hôpital pour comparaison
+    cy.get('.hospital-card')
+      .first()
+      .invoke('text')
+      .then((firstCardText) => {
+        cy.get('.pagination').contains('2').click();
+
+        // Vérifier que le contenu a changé après avoir cliqué sur la page 2
+        cy.get('.hospital-card')
+          .first()
+          .invoke('text')
+          .should((secondCardText) => {
+            expect(secondCardText).not.to.eq(firstCardText);
+          });
+      });
+  });
+});
