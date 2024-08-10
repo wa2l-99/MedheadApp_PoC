acb1a3c2e07abf016e233a854b8fa2db91971b95
wa2l-99 <zanoutr.1999@gmail.com>
2024-08-10 21:58:49 +0200

    fix : integrate Cypress e2e tests into CI pipeline with wait-on

--- poc-medhead-frontend/cypress.config.ts
+++ poc-medhead-frontend/cypress.config.ts
@@ -1,18 +1,17 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  
   e2e: {
-    'baseUrl': 'http://localhost:4200'
+    baseUrl: 'http://localhost:4200',
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000,
   },
-  
-  
+
   component: {
     devServer: {
       framework: 'angular',
       bundler: 'webpack',
     },
-    specPattern: '**/*.cy.ts'
-  }
-  
-})
+    specPattern: '**/*.cy.ts',
+  },
+});
