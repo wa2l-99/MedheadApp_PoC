a131c9df0518953c335496c3b5456d2a2b322065
wa2l-99 <zanoutr.1999@gmail.com>
2024-08-10 23:18:15 +0200

    fix pipeline frontend

--- poc-medhead-frontend/cypress.config.ts
+++ poc-medhead-frontend/cypress.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
-    defaultCommandTimeout: 10000,
-    pageLoadTimeout: 60000,
+    defaultCommandTimeout: 30000, // Augmenté à 30 secondes
+    pageLoadTimeout: 120000, // Augmenté à 2 minutes
+    requestTimeout: 30000, // Ajouté, 30 secondes pour les requêtes
+    responseTimeout: 60000, // Ajouté, 1 minute pour les réponses
   },
 
   component: {
@@ -13,5 +15,10 @@ export default defineConfig({
       bundler: 'webpack',
     },
     specPattern: '**/*.cy.ts',
+  },
+
+  retries: {
+    runMode: 2, // Ajoute 2 tentatives en mode run
+    openMode: 0,
   },
 });
