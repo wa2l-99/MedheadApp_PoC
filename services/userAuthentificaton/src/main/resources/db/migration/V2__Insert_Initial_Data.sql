c3602466df26db76b7042883f37cc1a50a0008a6
wa2l-99 <zanoutr.1999@gmail.com>
2024-08-20 14:35:26 +0200

    fix: Modify the insert migration file for the auth ms

--- services/userAuthentificaton/src/main/resources/db/migration/V2__Insert_Initial_Data.sql
+++ services/userAuthentificaton/src/main/resources/db/migration/V2__Insert_Initial_Data.sql
@@ -9,19 +9,29 @@ ON CONFLICT (nom) DO NOTHING;
 
 -- Insert users
 INSERT INTO users (nom, prenom, date_naissance, email, sexe, adresse, numero, password, account_locked, enabled, created_date)
-VALUES ('Admin', 'User', '1990-01-01', 'admin@example.com', 'Male', 'Admin Address', '1234567890','$2y$10$KeLphdCMw74r1vcOBCP9o.BKakhYrMx/gFdwYO80u1ozjurLruUn6', FALSE, TRUE, CURRENT_TIMESTAMP);
+VALUES ('Admin', 'User', '1990-01-01', 'admin@example.com', 'Male', 'Admin Address', '1234567890','$2y$10$KeLphdCMw74r1vcOBCP9o.BKakhYrMx/gFdwYO80u1ozjurLruUn6', FALSE, TRUE, CURRENT_TIMESTAMP)
+ON CONFLICT (email) DO NOTHING;
 
 INSERT INTO users (nom, prenom, date_naissance, email, sexe, adresse, numero, password, account_locked, enabled, created_date)
-VALUES ('Patient', 'User', '1995-01-01', 'patient@example.com', 'Female', 'Patient Address', '0987654321', '$2y$10$G0l4vshvH0fLtlJ9wn2F1OmQGZq0WyC9rWKSiPHGhRfmSf8Q1nNjS', FALSE, TRUE, CURRENT_TIMESTAMP);
+VALUES ('Patient', 'User', '1995-01-01', 'patient@example.com', 'Female', 'Patient Address', '0987654321', '$2y$10$G0l4vshvH0fLtlJ9wn2F1OmQGZq0WyC9rWKSiPHGhRfmSf8Q1nNjS', FALSE, TRUE, CURRENT_TIMESTAMP)
+ON CONFLICT (email) DO NOTHING;
 
 -- Assign roles to users
 INSERT INTO user_roles (user_id, role_id)
-VALUES ((SELECT id FROM users WHERE email = 'admin@example.com'), (SELECT id FROM role WHERE nom = 'Admin'));
+VALUES (
+           (SELECT id FROM users WHERE email = 'admin@example.com'),
+           (SELECT id FROM role WHERE nom = 'Admin')
+       )
+ON CONFLICT DO NOTHING;
 
 INSERT INTO user_roles (user_id, role_id)
-VALUES ((SELECT id FROM users WHERE email = 'patient@example.com'), (SELECT id FROM role WHERE nom = 'Patient'));
+VALUES (
+           (SELECT id FROM users WHERE email = 'patient@example.com'),
+           (SELECT id FROM role WHERE nom = 'Patient')
+       )
+ON CONFLICT DO NOTHING;
 
--- Générer et insérer des tokens pour les utilisateurs
+-- Generate and insert tokens for the users
 -- Admin token
 INSERT INTO token (token, created_at, expires_at, user_id)
 VALUES (
@@ -29,7 +39,8 @@ VALUES (
            CURRENT_TIMESTAMP,
            CURRENT_TIMESTAMP + INTERVAL '15 minutes',
            (SELECT id FROM users WHERE email = 'admin@example.com')
-       );
+       )
+ON CONFLICT DO NOTHING;
 
 -- Patient token
 INSERT INTO token (token, created_at, expires_at, user_id)
@@ -38,5 +49,5 @@ VALUES (
            CURRENT_TIMESTAMP,
            CURRENT_TIMESTAMP + INTERVAL '15 minutes',
            (SELECT id FROM users WHERE email = 'patient@example.com')
-       );
-
+       )
+ON CONFLICT DO NOTHING;
